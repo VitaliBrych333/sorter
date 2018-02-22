88053427853d15f5ce7342636602f96acfec149c
Vitali Br <epamc@mail.ru>
2018-02-22 14:01:56 +0300

    sorter

--- src/index.js
+++ src/index.js
@@ -1,31 +1,70 @@
 class Sorter {
-  constructor() {
+  constructor(name) {
+    
+    this.name=[];
+    
     // your implementation
   }
 
   add(element) {
+    
+    this.name.push(element);
+    
+
     // your implementation
   }
 
   at(index) {
+    return this.name[index];
     // your implementation
   }
 
-  get length() {
+  get length() { 
+    return this.name.length;
     // your implementation
   }
 
   toArray() {
+    return this.name;
     // your implementation
   }
 
   sort(indices) {
-    // your implementation
+    if (indices.length==3) {
+      for (var i = 0; i < 3; i++) {
+        for (var j = 0; j < 3 - i - 1; j++) {
+            if (this.name[j] > this.name[j + 1]) {
+                // меняем элементы местами
+                var temp = this.name[j];
+                this.name[j] = this.name[j + 1];
+                this.name[j + 1] = temp;
+            }
+        }
+      }
+    }
+    if (indices[0] > indices[1]) {
+      var r = indices[1];
+      indices[1]=indices[0];
+      indices[0]=r;
+    };
+    if (this.name[indices[0]] > this.name[indices[1]]) {
+      var t = this.name[indices[1]];
+      this.name[indices[1]] = this.name[indices[0]];
+      this.name[indices[0]] = t; 
+    };
+    return this.name;  
+      // your implementation
   }
+
 
   setComparator(compareFunction) {
-    // your implementation
+
+   
+     this.name.sort(compareFunction);
+      
+    
+   
+
   }
 }
-
 module.exports = Sorter;
