b838f7b3f0f117f9eb6c779f3b3c1ea01a3c0cc1
VitaliBrych333 <36643236+VitaliBrych333@users.noreply.github.com>
2018-09-12 12:57:49 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,70 +1,61 @@
 class Sorter {
   constructor(name) {
-    
-    this.name=[];
-    
+    this.name = name;
+    this.array=[];
+    this.sortMethod = (left,right) => left-right;
     // your implementation
   }
 
   add(element) {
-    
-    this.name.push(element);
-    
-
-    // your implementation
+    return this.array.push(element);
+      // your implementation
   }
 
   at(index) {
-    return this.name[index];
+    return this.array[index];
     // your implementation
   }
 
   get length() { 
-    return this.name.length;
+    return this.array.length;
     // your implementation
   }
 
   toArray() {
-    return this.name;
+    return this.array;
     // your implementation
   }
 
   sort(indices) {
-    if (indices.length==3) {
-      for (var i = 0; i < 3; i++) {
-        for (var j = 0; j < 3 - i - 1; j++) {
-            if (this.name[j] > this.name[j + 1]) {
-                // меняем элементы местами
-                var temp = this.name[j];
-                this.name[j] = this.name[j + 1];
-                this.name[j + 1] = temp;
-            }
+    let length = indices.length;
+    indices.sort((left, right) => left - right);
+
+    if (length == 1) {
+      return this.array[indices[0]];
+    } else {
+        let tempArray = []; 
+             
+        for (let i = 0; i < length; i++) { 
+          tempArray[i] = this.array[indices[i]];
         }
+      
+        tempArray.sort(this.sortMethod);
+      
+        for (let i = 0; i < length; i++ ) {
+          this.array[indices[i]] = tempArray[i];
+        }
+  
+      return this.array; 
       }
     }
-    if (indices[0] > indices[1]) {
-      var r = indices[1];
-      indices[1]=indices[0];
-      indices[0]=r;
-    };
-    if (this.name[indices[0]] > this.name[indices[1]]) {
-      var t = this.name[indices[1]];
-      this.name[indices[1]] = this.name[indices[0]];
-      this.name[indices[0]] = t; 
-    };
-    return this.name;  
-      // your implementation
-  }
-
 
   setComparator(compareFunction) {
-
-   
-     this.name.sort(compareFunction);
-      
-    
-   
-
+    this.sortMethod = compareFunction;
+  }
+}
+module.exports = Sorter;
+  setComparator(compareFunction) {
+    this.sortMethod = compareFunction;
   }
 }
 module.exports = Sorter;
